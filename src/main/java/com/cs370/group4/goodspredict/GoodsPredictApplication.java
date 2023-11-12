cdefedad8f866355af11eb6bd17e626e4ddc6579
satlin003 <satlin003@gmail.com>
2023-11-12 01:19:37 -0500

    built program

---
+++ src/main/java/com/cs370/group4/goodspredict/GoodsPredictApplication.java
@@ -0,0 +1,14 @@
+package com.cs370.group4.goodspredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GoodsPredictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsPredictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs370/group4/goodspredict/GoodsPredictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs370.group4.goodspredict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsPredictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
