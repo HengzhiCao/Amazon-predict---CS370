8ebe4f403ca1409d8945173d9ee0ccaad4d9e3b3
Hengzhi Cao <cao1092813874@gmail.com>
2023-12-05 15:56:33 -0500

    Update

--- src/main/java/com/cs370/group4/goodspredict/Split.java
+++
@@ -1,6 +0,0 @@
-package com.cs370.group4.goodspredict;
-
-public class Split {
-    int featureIndex;
-    double threshold;
-}
--- src/main/java/com/cs370/group4/goodspredict/TreeNode.java
+++
@@ -1,9 +0,0 @@
-package com.cs370.group4.goodspredict;
-
-public class TreeNode {
-    double threshold;
-    TreeNode left;
-    TreeNode right;
-    String label;
-    int featureIndex;
-}
