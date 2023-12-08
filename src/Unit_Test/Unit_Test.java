b25908a9e93f33c516f2111e51f333aa0c287069
Hengzhi Cao <cao1092813874@gmail.com>
2023-12-08 13:26:36 -0500

    Update

--- src/Unit_Test/Unit_Test.java
+++ src/Unit_Test/Unit_Test.java
@@ -209,6 +209,42 @@ public class Unit_Test {
         List<Instance> data = new ArrayList<>();
 
 
+        data.add(new Instance(new double[]{20, 40, 0.50, 4.5, 100}, "Yes"));
+        data.add(new Instance(new double[]{35, 50, 0.30, 3.8, 50}, "No"));
+        data.add(new Instance(new double[]{45, 60, 0.25, 4.0, 90}, "Yes"));
+        data.add(new Instance(new double[]{30, 45, 0.33, 4.2, 70}, "No"));
+        data.add(new Instance(new double[]{55, 70, 0.21, 3.9, 60}, "Yes"));
+        data.add(new Instance(new double[]{60, 80, 0.15, 4.1, 120}, "No"));
+        data.add(new Instance(new double[]{70, 90, 0.10, 4.0, 150}, "Yes"));
+        data.add(new Instance(new double[]{130, 170, 0.09, 3.8, 260}, "No"));
+        data.add(new Instance(new double[]{140, 180, 0.07, 3.9, 280}, "Yes"));
+        data.add(new Instance(new double[]{150, 190, 0.05, 3.7, 300}, "No"));
+        data.add(new Instance(new double[]{160, 200, 0.03, 3.8, 320}, "Yes"));
+        data.add(new Instance(new double[]{170, 210, 0.01, 3.9, 340}, "No"));
+        data.add(new Instance(new double[]{180, 220, 0.02, 2.8, 360}, "No"));
+        data.add(new Instance(new double[]{190, 230, 0.00, 2.9, 380}, "Yes"));
+        data.add(new Instance(new double[]{200, 240, 0.00, 2.7, 400}, "No"));
+        data.add(new Instance(new double[]{210, 250, 0.00, 2.8, 420}, "Yes"));
+        data.add(new Instance(new double[]{220, 260, 0.00, 2.9, 440}, "No"));
+        data.add(new Instance(new double[]{200, 1200, 0.1667, 4.7, 200}, "Yes"));
+        data.add(new Instance(new double[]{250, 1500, 0.1667, 4.9, 300}, "Yes"));
+        data.add(new Instance(new double[]{300, 1800, 0.1667, 4.6, 250}, "No"));
+        data.add(new Instance(new double[]{350, 2000, 0.175, 4.8, 400}, "Yes"));
+        data.add(new Instance(new double[]{450, 2250, 0.20, 4.4, 150}, "No"));
+        data.add(new Instance(new double[]{400, 2500, 0.6, 4.5, 200}, "Yes"));
+        data.add(new Instance(new double[]{500, 3000, 0.65, 4.8, 300}, "Yes"));
+        data.add(new Instance(new double[]{600, 3500, 0.7, 4.6, 250}, "No"));
+        data.add(new Instance(new double[]{700, 4000, 0.75, 4.7, 200}, "Yes"));
+        data.add(new Instance(new double[]{800, 4500, 0.8, 4.8, 300}, "Yes"));
+        data.add(new Instance(new double[]{900, 5000, 0.85, 4.6, 250}, "No"));
+        data.add(new Instance(new double[]{230, 270, 0.02, 3.7, 360}, "Yes"));
+        data.add(new Instance(new double[]{240, 280, 0.01, 3.8, 380}, "No"));
+        data.add(new Instance(new double[]{250, 290, 0.00, 3.9, 400}, "Yes"));
+        data.add(new Instance(new double[]{260, 300, 0.00, 3.7, 420}, "No"));
+        data.add(new Instance(new double[]{270, 310, 0.00, 3.8, 440}, "Yes"));
+
+
+
         return data;
     }
 
