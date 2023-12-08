121cd27146f7a757833930b1196801d4db0be8f3
Hengzhi Cao <cao1092813874@gmail.com>
2023-12-08 13:38:07 -0500

    Update comment

--- src/Model_classifier/RandomForestModel.java
+++ src/Model_classifier/RandomForestModel.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RandomForestModel {
     private RandomForest randomForest;
